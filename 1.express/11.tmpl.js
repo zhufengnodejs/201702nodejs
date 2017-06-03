32057157bf2108d062ae0cc8f121e77c0248ebff
zhufengnodejs <zhufengnodejs@126.com>
2017-06-03 15:55:08 +0800

    本地安装express

--- 1.express/11.tmpl.js
+++ 1.express/11.tmpl.js
@@ -7,10 +7,17 @@ app.set('view engine','html');
 //模板存放的根目录
 app.set('views',path.resolve('tmpl'));
 app.engine('html',require('ejs').__express);
+app.use(function(req,res,next){
+  //res.locals才是真正渲染模板的数据对象
+  res.locals.website = '珠峰培训';
+  // {website:'珠峰培训'}
+  next();
+});
 app.get('/',function(req,res){
     //模板+数据对象
     //render负责把模板文件和数据对象进行混合并响应输出
     //1参数是模板的相对路径 2数据对象
+    //在真正渲染之前会把 {website:'珠峰培训',title:'首页'}
     res.render('home',{title:'首页'});
 });
 app.get('/user',function(req,res){
