a3b62ee02a7663f7229aa538c424812e8220924c
zhufengnodejs <zhufengnodejs@126.com>
2017-06-03 16:20:15 +0800

    本地安装express

---
+++ 1.express/12.static.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let path = require('path');
+let fs = require('fs');
+let app = express();
+//root是静态文件根目录
+// /css/index.css
+function static(root){
+  return function(req,res,next){
+// E:\201702nodejs\1.express\public/css/index.css
+    let realPath = path.join(root,req.path);
+    fs.exists(realPath,function(exists){
+        if(exists){
+            res.sendFile(realPath);
+        }else{
+            next();
+        }
+    })
+  }
+}
+app.use(static(path.resolve('public')));
+//静态文件中间件-用来响应对客户端的静态文件请求
+app.get('/',function(req,res){
+    //path must be absolute or specify root to res.sendFile
+    //res.sendFile('./index.html',{root:__dirname});
+    res.sendFile(path.resolve('./index.html'));
+});
+
+
+app.listen(8080);
