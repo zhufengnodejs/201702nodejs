bc68faaf14a4c55b9150460b48af83f4cf58e746
zhufengnodejs <zhufengnodejs@126.com>
2017-06-04 18:01:20 +0800

    cookie

--- 3.login/server.js
+++ 3.login/server.js
@@ -3,7 +3,8 @@ let express = require('express');
 //bodyParser用来处理请求体，把请求体转成对象赋给req.body
 //username=admin&password=admin=>{username,password}
 let bodyParser = require('body-parser');
-let cookieParser = require('cookie-parser');
+let session = require('express-session');
+let MongoStore = require('connect-mongo')(session);
 let app = express();
 //输入的用户名和密码都是admin就认为登录成功
 //设置模板引擎
@@ -14,7 +15,16 @@ app.engine('html', require('ejs').__express);
 // req.body
 app.use(bodyParser.urlencoded({extended: true}));
 //req.cookies = querystring.parse(req.header.cookie,'; ')
-app.use(cookieParser());
+// req.session
+app.use(session({
+    secret:'zfpx',
+    resave:true,
+    saveUninitialized:true,
+    //store就是指定会话的存储的位置
+    store:new MongoStore({
+        url:'mongodb://127.0.0.1/201702node'
+    })
+}));
 app.get('/user/signin', function (req, res) {
     res.render('signin', {title: '登录'});
 });
@@ -23,7 +33,8 @@ app.post('/user/signin', function (req, res) {
     if (user.username == 'admin' && user.password == 'admin') {
         //如果登录成功，向客户端写入cookie
         //res.setHeader('Set-Cookie','username=admin');
-        res.cookie('username',user.username);
+        //res.cookie('username',user.username);
+        req.session.username = user.username;
         res.redirect('/welcome');
     } else {
         res.redirect('back');
@@ -31,7 +42,7 @@ app.post('/user/signin', function (req, res) {
 });
 app.get('/welcome',function(req,res){
 //req.cookies = querystring.parse(req.header.cookie,'; ')
-    res.send(`欢迎${req.cookies.username}光临`);
+    res.send(`欢迎${req.session.username}光临`);
 });
 // action是提交的路径 如果不指定默认就是当前路径
 // method提交的方法
