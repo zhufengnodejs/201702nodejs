b0c8d6f0abbfa27f1a6713a8b16793ea342873f2
zhufengnodejs <zhufengnodejs@126.com>
2017-06-17 10:29:40 +0800

    启动服务器

--- 9.websocket/1.query.js
+++ 9.websocket/1.query.js
@@ -1,6 +1,27 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
-app.get('/clock',function(req,res){
+//我们已经让bodyParser插件解析json格式的请求体了
+//是根据请求头中的Content-Type
+//app.use(bodyParser.json());
+//如果没有指定请求头中的内容类型，那么格式就是纯文本的
+app.use(bodyParser.text());
+let messages = [];
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    if(req.method == 'OPTIONS'){
+        res.end();
+    }else{
+        next();
+    }
+});
+app.post('/message',function(req,res){
+    console.log(req.body);
+    messages.push(req.body);
+    res.send(messages);
+});
+app.get('/message',function(req,res){
     /**
      * 跨域的响应头 都是以Access-Control-Allow-开头的
      * Access-Control-Allow-
@@ -8,7 +29,7 @@ app.get('/clock',function(req,res){
      * Headers 允许的请求头
      * Methods 允许客户端发送的方法
      */
- res.setHeader('Access-Control-Allow-Origin','*');
- res.send(new Date().toLocaleString());
+
+ res.send(messages);
 });
 app.listen(3000);
