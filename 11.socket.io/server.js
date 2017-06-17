27f0fe7ee16e36c37513a56b5f0b493d4b336625
zhufengnodejs <zhufengnodejs@126.com>
2017-06-17 15:29:42 +0800

    启动服务器

--- 11.socket.io/server.js
+++ 11.socket.io/server.js
@@ -5,6 +5,8 @@
  **/
 let express = require('express');
 let app = express();
+let path = require('path');
+app.use(express.static(path.resolve('../node_modules')));
 app.get('/',function(req,res){
     res.sendFile('./index.html',{root:__dirname});
 });
